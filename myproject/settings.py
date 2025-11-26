50b3724974194188f93ccca77ab6b163697da479
farahnasri <nareej777@gmail.com>
2025-11-26 12:04:21 +0100

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,12 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-
-# Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # dossier où collecter les static files pour prod
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
